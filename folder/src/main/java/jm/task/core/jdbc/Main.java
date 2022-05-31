f6aa3f4e003169e37b5ae85b310e0daa1467bf30
Mariia <98399769+mashamaalova@users.noreply.github.com>
2022-05-31 15:52:29 +0300

    Add files via upload

---
+++ folder/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,34 @@
+package jm.task.core.jdbc;
+
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("Name1", "LastName1", (byte) 20);
+        userDao.saveUser("Name2", "LastName2", (byte) 25);
+        userDao.saveUser("Name3", "LastName3", (byte) 31);
+        userDao.saveUser("Name4", "LastName4", (byte) 38);
+
+        userDao.removeUserById(1);
+        userDao.getAllUsers();
+        for (User user : userDao.getAllUsers()) {
+            System.out.println(user);
+        }
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
+
+
+    }
+}
